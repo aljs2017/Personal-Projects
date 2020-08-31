bc4f12dfff4062ca84534780d8a45fe5cbb35741
aljs2017 <63164834+aljs2017@users.noreply.github.com>
2020-08-30 21:26:17 -0500

    Add files via upload

--- Count Primes.py
+++ Count Primes.py
@@ -4,26 +4,27 @@ print("Please enter the input number.")
 
 input_number = int(input())
 
-prime_count = 1
+prime_count = 0
+
 
 def countPrimes(input_number, prime_count):
-    for number in range(1, input_number):
+    for number in range(1, input_number - 1):
         current_number = input_number - number
-        isPrime(current_number)
+        #        isPrime(current_number, prime_count)
 
-        if isPrime == True:
+        if isPrime(current_number, prime_count) == True:
             prime_count += 1
         else:
             continue
 
+    return prime_count
 
-def isPrime(current_number):
+
+def isPrime(current_number, prime_count):
     for division_number in range(2, current_number):
-        if division_number % current_number == 0:
+        if current_number % division_number == 0:
             return False
     return True
 
 
-
-
-
+countPrimes(input_number, prime_count)
